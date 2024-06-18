605a3f8b7babf8812b4423d23b52f0a5476bb5e1
Izanalgit <izanalvarez@gmail.com>
2024-06-18 16:10:58 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
- //Aquí tu código
+    const [name,newName] = useState('Sofia');
+
+    const names = ["Data", "Reyes", "Yolanda"];
+
+    return (
+        <>
+            <h2>Teach Name {name}</h2>
+            <ul>
+                {names.map(n=>
+                    <li>
+                        <button onClick={()=>newName(n)}>
+                            {n}
+                        </button>
+                    </li>
+                )}
+            </ul>
+        </>
+    )
 }
 
 export default App
